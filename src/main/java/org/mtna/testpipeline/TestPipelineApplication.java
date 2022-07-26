520007dde81b4a81014cfca486200b0acf58ce0e
yousefipour <amin.yousefipour@gmail.com>
2022-07-26 08:12:28 +0430

    first commit

---
+++ src/main/java/org/mtna/testpipeline/TestPipelineApplication.java
@@ -0,0 +1,13 @@
+package org.mtna.testpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPipelineApplication.class, args);
+    }
+
+}
